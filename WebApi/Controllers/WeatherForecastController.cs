c89e3fb8c75ed6ccc5113465788abb6427fe30dc
Prateek Xaxa <prateekxaxa@microsoft.com>
2021-09-11 20:31:17 +0530

    Initial push

---
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration configuration;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            string host = configuration.GetSection("appSettings")["host"].ToString();
+            string port = configuration.GetSection("appSettings")["port"].ToString();
+            Console.WriteLine($"host {host}, port {port}");
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
